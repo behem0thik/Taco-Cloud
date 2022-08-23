c84ebe97c4a0b1adb80ab133f495fef39ebc5198
behem0thik <behem0thik@users.noreply.github.com>
2022-08-23 11:00:03 +0600

    Chapter 3.1.4 Saving data

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 import javax.validation.Valid;
 
@@ -17,6 +18,12 @@ import javax.validation.Valid;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+    private OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @GetMapping("/current")
     public String orderForm() {
@@ -29,8 +36,10 @@ public class OrderController {
         if (errors.hasErrors()) {
             return "orderForm";
         }
-        log.info("Order submitted: {}", order);
+        orderRepository.save(order);
+        log.info("Processing Order: {}", order);
         sessionStatus.setComplete();
+
         return "redirect:/";
     }
 
