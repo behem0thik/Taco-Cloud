6dbd22ad7059e3b3f55b179841b924dd78a3ffda
behem0thik <behem0thik@users.noreply.github.com>
2022-08-27 10:18:14 +0600

    Chapter 3.3.2 Annotating Domain Data Classes

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -1,26 +1,34 @@
 package tacos;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class Taco {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date createdAt = new Date();
+
     @NotNull
     @Size(min = 5, message = "Name mast be at least 5 characters long")
     private String name;
-    @NotNull
+
+    @ManyToMany
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<IngredientRef> ingredients;
+    private List<Ingredient> ingredients = new ArrayList<>();
 //    private List<Ingredient> ingredients; так было в книжке
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
