210efb6bca13430579a173a1793c44e9b402ecdb
behem0thik <behem0thik@users.noreply.github.com>
2022-08-27 10:20:33 +0600

    Chapter 3.3.3 Declaring JPA repositories

--- src/main/java/tacos/data/IngredientRepository.java
+++ src/main/java/tacos/data/IngredientRepository.java
@@ -3,5 +3,6 @@ package tacos.data;
 import org.springframework.data.repository.CrudRepository;
 import tacos.Ingredient;
 
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+public interface IngredientRepository
+        extends CrudRepository<Ingredient, String> {
 }
