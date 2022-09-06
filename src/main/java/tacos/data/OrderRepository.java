ce05a20454e5f7c6f693e840fc02b3c65d0be871
behem0thik <behem0thik@users.noreply.github.com>
2022-09-06 11:00:02 +0600

    Chapter 4.1.4 Defining Cassandra repositories

--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import tacos.entity.TacoOrder;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface OrderRepository
-        extends CrudRepository<TacoOrder, Long> {
+        extends CrudRepository<TacoOrder, UUID> {
 
     List<TacoOrder> findByDeliveryZip(String deliveryZip);
 
