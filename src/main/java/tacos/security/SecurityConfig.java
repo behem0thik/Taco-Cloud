bad492aaf05ccd4d6e4c302a938d12f08c129fe4
behem0thik <behem0thik@users.noreply.github.com>
2022-09-15 10:21:06 +0600

    fix bean

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() throws Exception {
+    public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().antMatchers("/h2-console/**");
     }
 
